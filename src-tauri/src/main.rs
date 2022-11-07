17d7588b15199cf809a1ce2963c7689296571752
Koudocko <tyler@pcquest.ca>
2022-11-07 16:33:58 -0500

    Created backend command to be called by frontend.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -13,6 +13,23 @@ lazy_static!{
     static ref DATABASE: Mutex<Option<Range<DataType>>> = Mutex::new(None);
 }
 
+fn lookup_product(lpn: &str)-> Result<String, ()>{Err(())}
+fn scrape_data(body: &str)-> Result<(String, String), ()>{Err(())}
+
+#[tauri::command]
+async fn get_product(lpn: String)-> (String, String){
+    if let Ok(asin) = lookup_product(&lpn){
+        if let Ok(body) = reqwest::get(format!("https://amazon.com/dp/{}", asin))
+            .await.unwrap().text().await{
+            if let Ok(data) = scrape_data(&body){
+                return data;
+            }
+        }
+    }
+
+    (String::from("No Results Found"), String::new())
+}
+
 fn main() {
     if DATABASE.lock().unwrap().is_none(){
         // Load excel database
